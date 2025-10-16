b306ccc61bb653214b14c862d852b33aa6d693ba
Mulugeta Tesfaye <mulugetatesfaye081@gmail.com>
2025-10-16 21:20:19 +0300

    Add users page

---
+++ app/users/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface User {
+    id: number
+    name: string
+}
+
+const TestPage = async () => {
+
+const res = await fetch('https://jsonplaceholder.typicode.com/users')
+const users:User[] = await res.json()
+  return (
+    <>
+      <h1>users</h1>
+      <ul>
+        {users.map((user)=> <li key={user.id}>{user.name}</li>)}
+      </ul>
+      
+    </>
+  )
+}
+
+export default TestPage
